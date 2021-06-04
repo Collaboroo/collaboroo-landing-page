87a79b4d556f7779ef004c9f3d1aebf09c96fada
Stephen Alvin <stephenalvin0912@gmail.com>
2021-06-05 02:49:06 +0800

    Added Links to Google Form

--- src/components/layout/Header.js
+++ src/components/layout/Header.js
@@ -120,7 +120,7 @@ const Header = ({
                       className="list-reset header-nav-right"
                     >
                       <li>
-                        <Link to="#0" className="button button-primary button-wide-mobile button-sm" onClick={closeMenu}>Sign up</Link>
+                        <a href="https://forms.gle/HrMvkB4ucAZ8jtRv7" className="button button-primary button-wide-mobile button-sm" onClick={closeMenu}>Try Beta</a>
                       </li>
                     </ul>}
                 </div>
--- src/components/sections/Hero.js
+++ src/components/sections/Hero.js
@@ -69,8 +69,8 @@ const Hero = ({
                 </p>
               <div className="reveal-from-bottom" data-reveal-delay="600">
                 <ButtonGroup>
-                  <Button tag="a" color="primary" wideMobile href="https://cruip.com/">
-                    Sign Up For Beta
+                  <Button tag="a" color="primary" wideMobile href="https://forms.gle/HrMvkB4ucAZ8jtRv7">
+                    Try Beta
                     </Button>
                   {/* <Button tag="a" color="dark" wideMobile href="https://github.com/cruip/open-react-template/">
                     View on Github
