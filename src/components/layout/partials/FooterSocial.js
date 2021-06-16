3bfdec8764696ab6d6b87fa0d665ca45998eaa2c
Stephen Alvin <stephenalvin0912@gmail.com>
2021-06-17 03:03:33 +0800

    Added methodology

--- src/components/layout/partials/FooterSocial.js
+++ src/components/layout/partials/FooterSocial.js
@@ -39,7 +39,7 @@ const FooterSocial = ({
           </a>
         </li>
         <li>
-          <a href="https://www.instagram.com/collaboroo/">
+          {/* <a href="https://www.instagram.com/collaboroo/">
             <svg
               width="24"
               height="24"
@@ -54,7 +54,7 @@ const FooterSocial = ({
                   d="M12 16H4c-2.056 0-4-1.944-4-4V4c0-2.056 1.944-4 4-4h8c2.056 0 4 1.944 4 4v8c0 2.056-1.944 4-4 4zM4 2c-.935 0-2 1.065-2 2v8c0 .953 1.047 2 2 2h8c.935 0 2-1.065 2-2V4c0-.935-1.065-2-2-2H4z" />
               </g>
             </svg>
-          </a>
+          </a> */}
         </li>
       </ul>
     </div>
--- src/components/sections/FeaturesTiles.js
+++ src/components/sections/FeaturesTiles.js
@@ -63,7 +63,7 @@ const FeaturesTiles = ({
               <div className="tiles-item-inner">
                 <div style={{ marginBottom: "16px" }}>
                   <Image
-                    src={require('./../../assets/images/collaboroo/icon_time.svg')}
+                    src={require('./../../assets/images/collaboroo/icon_recruit.svg')}
                     alt="Features tile icon 01"
                     width={64}
                     height={64}
@@ -71,11 +71,11 @@ const FeaturesTiles = ({
                 </div>
                 <div className="features-tiles-item-content">
                   <h4 className="mt-0 mb-8">
-                    Save Time
-                    </h4>
+                    Recruit
+                  </h4>
                   <p className="m-0 text-sm">
-                    Engage your customers asynchronously. Spend more time on the work that actually matters
-                    </p>
+                    Innovative web scraping technologies to recruit the right respondents faster.
+                  </p>
                 </div>
               </div>
             </div>
@@ -91,11 +91,11 @@ const FeaturesTiles = ({
                 </div>
                 <div className="features-tiles-item-content">
                   <h4 className="mt-0 mb-8">
-                    Qualitative Data
-                    </h4>
+                    Screen
+                  </h4>
                   <p className="m-0 text-sm">
-                    Open-ended video responses from your customers transcribed for your convenience.
-                    </p>
+                    Distractor questions and other behavioural science methodologies to double check the quality of our participants
+                  </p>
                 </div>
               </div>
             </div>
@@ -104,21 +104,62 @@ const FeaturesTiles = ({
               <div className="tiles-item-inner">
                 <div style={{ marginBottom: "16px" }}>
                   <Image
-                    src={require('./../../assets/images/collaboroo/icon_share.svg')}
+                    src={require('./../../assets/images/collaboroo/icon_time.svg')}
                     alt="Features tile icon 03"
                     width={64}
                     height={64} />
                 </div>
                 <div className="features-tiles-item-content">
                   <h4 className="mt-0 mb-8">
-                    Share Feedback
-                    </h4>
+                    Schedule
+                  </h4>
                   <p className="m-0 text-sm">
-                    Team-wide access to customer feedback data. No more needing the whole team for every interview
-                    </p>
+                    Scheduling the right time with respondents can be a big pain. Calender integration to make scheduling hassle free!
+                  </p>
                 </div>
               </div>
             </div>
+
+            <div className="tiles-item reveal-from-bottom" data-reveal-delay="400">
+              <div className="tiles-item-inner">
+                <div style={{ marginBottom: "16px" }}>
+                  <Image
+                    src={require('./../../assets/images/collaboroo/icon_reviews.svg')}
+                    alt="Features tile icon 03"
+                    width={64}
+                    height={64} />
+                </div>
+                <div className="features-tiles-item-content">
+                  <h4 className="mt-0 mb-8">
+                    Reviews
+                  </h4>
+                  <p className="m-0 text-sm">
+                    Reviews from our interviewers to assess the quality of respondents and improve our recruitment process.
+                  </p>
+                </div>
+              </div>
+            </div>
+
+            <div className="tiles-item reveal-from-bottom" data-reveal-delay="400">
+              <div className="tiles-item-inner">
+                <div style={{ marginBottom: "16px" }}>
+                  <Image
+                    src={require('./../../assets/images/collaboroo/icon_reward.svg')}
+                    alt="Features tile icon 03"
+                    width={64}
+                    height={64} />
+                </div>
+                <div className="features-tiles-item-content">
+                  <h4 className="mt-0 mb-8">
+                    Token of Appreciation
+                  </h4>
+                  <p className="m-0 text-sm">
+                   We reward our respondents while ensuring quality so you don't have to worry about your budget.
+                  </p>
+                </div>
+              </div>
+            </div>
+
           </div>
         </div>
       </div>
